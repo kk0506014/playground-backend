b6e14313df7f9595840bfffce3575c3447445831
kk0506014 <kk0506014@gmail.com>
2025-08-19 02:10:32 +0900

    feat: 유저 엔티티 구현

--- src/main/java/com/playground/backend/domain/user/entity/User.java
+++ src/main/java/com/playground/backend/domain/user/entity/User.java
@@ -1,11 +1,34 @@
 package com.playground.backend.domain.user.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 유저 엔티티
  */
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, unique = true, nullable = false)
+    private String email;
+
+    @Column(length = 255, nullable = false)
+    private String password;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "profile_image")
+    private String profileImage;
 }
