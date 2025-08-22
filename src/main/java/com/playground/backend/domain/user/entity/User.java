792a2597f8482a9bb77cd3f379be43ff0ed9f572
kk0506014 <kk0506014@gmail.com>
2025-08-22 21:43:35 +0900

    refactor: 유저 엔티티에 비밀번호 컬럼 제약 수정

--- src/main/java/com/playground/backend/domain/user/entity/User.java
+++ src/main/java/com/playground/backend/domain/user/entity/User.java
@@ -24,7 +24,7 @@ public class User extends BaseEntity {
     @Column(length = 100, unique = true, nullable = false)
     private String email;
 
-    @Column(length = 255, nullable = false)
+    @Column(nullable = false)
     private String password;
 
     @Column(length = 100, nullable = false)
