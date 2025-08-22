8246b14ce66af5291aadc083d31cfde076361844
kk0506014 <kk0506014@gmail.com>
2025-08-22 21:45:13 +0900

    feat: 스포츠 타입 엔티티 생성

---
+++ src/main/java/com/playground/backend/domain/sporttype/entity/SportType.java
@@ -0,0 +1,25 @@
+package com.playground.backend.domain.sporttype.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 스포츠 타입 엔티티
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SportType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, unique = true, nullable = false)
+    private String name;
+}
