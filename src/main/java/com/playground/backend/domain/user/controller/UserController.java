6b60f122df7928f9c945cf934d34f0ed70474aa7
kk0506014 <kk0506014@gmail.com>
2025-08-25 01:40:20 +0900

    feat: 유저 컨트롤러 생성

---
+++ src/main/java/com/playground/backend/domain/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.playground.backend.domain.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 유저 컨트롤러
+ */
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+}
