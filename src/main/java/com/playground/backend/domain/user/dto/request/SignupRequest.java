4b39ff79a75045f7c1772792b8b13a0cb2e1f4b8
kk0506014 <kk0506014@gmail.com>
2025-08-27 20:48:54 +0900

    feat: 회원가입 요청 DTO에 추가된 내용 반영

--- src/main/java/com/playground/backend/domain/user/dto/request/SignupRequest.java
+++ src/main/java/com/playground/backend/domain/user/dto/request/SignupRequest.java
@@ -1,5 +1,9 @@
 package com.playground.backend.domain.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +16,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignupRequest {
 
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8, message = "비밀번호는 8자리 이상이어야 합니다.")
+//    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+//            message = "비밀번호는 8자리 이상, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
-    private String name;
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    private String userName;
+
+    @NotBlank(message = "이름을 입력해주세요.")
+    private String fullName;
+
+    @NotBlank(message = "전화번호를 입력해주세요.")
+    @Pattern(regexp = "\\d{3}-\\d{4}-\\d{4}", message = "전화번호 형식이 올바르지 않습니다. (ex: 010-1234-5678)")
+    private String phoneNumber;
 
     private String profileImage;
 }
