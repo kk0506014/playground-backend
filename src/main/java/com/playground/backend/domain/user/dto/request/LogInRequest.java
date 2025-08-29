5e8ecac789eaefc87ea9fd14c4c140b06782eb60
kk0506014 <kk0506014@gmail.com>
2025-08-29 21:54:35 +0900

    feat: 로그인 요청 DTO LogInRequest 생성

---
+++ src/main/java/com/playground/backend/domain/user/dto/request/LogInRequest.java
@@ -0,0 +1,23 @@
+package com.playground.backend.domain.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 로그인 요청 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogInRequest {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+}
