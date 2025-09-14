3951702c962bdfe84c1b91e59ea18bd2f16296e7
kk0506014 <kk0506014@gmail.com>
2025-09-15 01:38:36 +0900

    feat: CORS 설정(비활성화 상태) 추가

--- src/main/java/com/playground/backend/global/config/SecurityConfig.java
+++ src/main/java/com/playground/backend/global/config/SecurityConfig.java
@@ -65,6 +65,29 @@ public class SecurityConfig {
     }
 
     /**
+     * CORS 설정
+     *
+     * 프론트엔드에서 API 호출 시 다른 도메인 접근 허용
+     * 쿠키/인증 정보 전송 허용, 필요한 HTTP 메서드 및 헤더 설정
+     *
+     * @return CORS 설정이 적용된 CorsConfigurationSource
+     */
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        configuration.setAllowCredentials(true); // 클라이언트가 쿠키 및 인증 정보 전송 허용
+//        configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 프론트엔드 주소
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")); // HTTP 메서드 허용
+//        configuration.addAllowedHeader("*"); // 요청 헤더 허용
+//        configuration.addExposedHeader("*"); // 헤더 접근 가능
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+
+    /**
      * PasswordEncoder Bean 등록
      */
     @Bean
