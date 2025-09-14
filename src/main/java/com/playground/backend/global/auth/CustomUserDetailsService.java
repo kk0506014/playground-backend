9af13c7a3e92cfb124f68b981c26c6e420698762
kk0506014 <kk0506014@gmail.com>
2025-09-15 01:33:38 +0900

    chore: CustomUserDetailsService 활성화 및 주석 생성

--- src/main/java/com/playground/backend/global/auth/CustomUserDetailsService.java
+++ src/main/java/com/playground/backend/global/auth/CustomUserDetailsService.java
@@ -1,22 +1,30 @@
-//package com.playground.backend.global.auth;
-//
-//import com.playground.backend.domain.user.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CustomUserDetailsService implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String email) {
-//        return userRepository.findByEmail(email)
-//                .map(CustomUserDetails::new)
-//                .orElse(null);
-//    }
-//
-//}
+package com.playground.backend.global.auth;
+
+import com.playground.backend.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+/**
+ * CustomUserDetails 서비스
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * UserDetails 조회 메서드
+     *
+     * @param email 사용자 이메일
+     * @return UserDetails 객체(CustomUserDetails), 조회 실패 시 null 반환
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .map(CustomUserDetails::new)
+                .orElse(null);
+    }
+}
