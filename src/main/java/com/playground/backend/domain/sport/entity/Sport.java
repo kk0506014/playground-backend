e98250d16b1e3d0987060e67b2a0de42be2443e7
kk0506014 <kk0506014@gmail.com>
2025-09-18 02:27:38 +0900

    refactor: Sport 엔티티에서 minTeamMember, maxTeamMember, maxTeamMember 제거

--- src/main/java/com/playground/backend/domain/sport/entity/Sport.java
+++ src/main/java/com/playground/backend/domain/sport/entity/Sport.java
@@ -23,13 +23,4 @@ public class Sport {
 
     @Column(length = 100, unique = true, nullable = false)
     private String name;
-
-    @Column(nullable = false)
-    private Integer minTeamMember;
-
-    @Column(nullable = false)
-    private Integer maxTeamMember;
-
-    @Column(nullable = false)
-    private Integer playersOnField;
 }
