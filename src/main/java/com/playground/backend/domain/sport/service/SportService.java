c8a77f932996a6ec7722ba3cf23daf8b858268e0
kk0506014 <kk0506014@gmail.com>
2025-09-18 01:51:42 +0900

    feat: 스포츠 서비스 생성

---
+++ src/main/java/com/playground/backend/domain/sport/service/SportService.java
@@ -0,0 +1,12 @@
+package com.playground.backend.domain.sport.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * 스포츠 서비스
+ */
+@RequiredArgsConstructor
+@Service
+public class SportService {
+}
