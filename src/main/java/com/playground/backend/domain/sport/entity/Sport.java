a709e270d20d3873780d2387fffca993b22e4392
kk0506014 <kk0506014@gmail.com>
2025-09-17 18:40:22 +0900

    feat: 스포츠 엔티티에 maxTeamMember, playersOnField 추가 구현

--- src/main/java/com/playground/backend/domain/sport/entity/Sport.java
+++ src/main/java/com/playground/backend/domain/sport/entity/Sport.java
@@ -24,6 +24,12 @@ public class Sport {
     @Column(length = 100, unique = true, nullable = false)
     private String name;
 
-    @Column(name = "min_team_member", nullable = false)
+    @Column(nullable = false)
     private Integer minTeamMember;
+
+    @Column(nullable = false)
+    private Integer maxTeamMember;
+
+    @Column(nullable = false)
+    private Integer playersOnField;
 }
