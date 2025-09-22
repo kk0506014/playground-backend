2a11b658e137211491a8ac755fa89005fdd6a810
kk0506014 <kk0506014@gmail.com>
2025-09-22 15:42:40 +0900

    feat: 스포츠 생성 메서드 구현

--- src/main/java/com/playground/backend/domain/sport/service/SportService.java
+++ src/main/java/com/playground/backend/domain/sport/service/SportService.java
@@ -1,7 +1,11 @@
 package com.playground.backend.domain.sport.service;
 
+import com.playground.backend.domain.sport.dto.request.CreateSportRequest;
+import com.playground.backend.domain.sport.entity.Sport;
+import com.playground.backend.domain.sport.repository.SportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 스포츠 서비스
@@ -9,4 +13,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class SportService {
+
+    private final SportRepository sportRepository;
+
+    /**
+     * 스포츠 생성 메서드
+     *
+     * @param createSportRequest 스포츠 생성 요청 DTO
+     */
+    @Transactional
+    public void createSport(CreateSportRequest createSportRequest) {
+
+        Sport sport = Sport.builder()
+                .name(createSportRequest.getName())
+                .build();
+
+        sportRepository.save(sport);
+    }
 }
