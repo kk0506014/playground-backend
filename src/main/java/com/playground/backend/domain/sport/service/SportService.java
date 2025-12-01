c7ea6b2955bdfbe6971ff3b6d1120b3ac025f8d0
kk0506014 <kk0506014@gmail.com>
2025-12-01 18:39:52 +0900

    feat: 생성 메서드에 minTeamMember, maxTeamMember 추가

--- src/main/java/com/playground/backend/domain/sport/service/SportService.java
+++ src/main/java/com/playground/backend/domain/sport/service/SportService.java
@@ -2,6 +2,8 @@ package com.playground.backend.domain.sport.service;
 
 import com.playground.backend.domain.sport.dto.request.CreateSportRequest;
 import com.playground.backend.domain.sport.entity.Sport;
+import com.playground.backend.domain.sport.exception.SportErrorCode;
+import com.playground.backend.domain.sport.exception.SportException;
 import com.playground.backend.domain.sport.repository.SportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,12 +22,23 @@ public class SportService {
      * 스포츠 생성 메서드
      *
      * @param createSportRequest 스포츠 생성 요청 DTO
+     * @throws SportException NAME_EXISTS
+     * @throws SportException INVALID_TEAM_MEMBER
      */
     @Transactional
     public void createSport(CreateSportRequest createSportRequest) {
+        if (sportRepository.existsByName(createSportRequest.getName())) {
+            throw new SportException(SportErrorCode.NAME_EXISTS);
+        }
+
+        if (createSportRequest.getMinTeamMember() > createSportRequest.getMaxTeamMember()) {
+            throw new SportException(SportErrorCode.INVALID_TEAM_MEMBER);
+        }
 
         Sport sport = Sport.builder()
                 .name(createSportRequest.getName())
+                .minTeamMember(createSportRequest.getMinTeamMember())
+                .maxTeamMember(createSportRequest.getMaxTeamMember())
                 .build();
 
         sportRepository.save(sport);
